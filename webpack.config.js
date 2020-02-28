7601dff087d189f9fd4421d1cfdd991bef845700
Joshua Ashmall-Liversidge <jliversidge@appacademy.io>
2020-02-27 19:40:13 -0500

    implemented minheap

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
-    
-}
+    entry: './lib/solver.js',
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'bundle.js'
+    }
+};
