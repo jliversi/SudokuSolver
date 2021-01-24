64fd940aa4a97866000eaed6691139d7cc6773dd
Joshua Ashmall-Liversidge <jliveridge1@gmail.com>
2021-01-24 17:08:22 -0500

    draft 2 done for non-rec solve

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './lib/solver.js',
+    entry: './lib/index.js',
     output: {
         path: path.resolve(__dirname),
         filename: 'bundle.js'
